4360987fa0e95ae64f67f2c3f5b33c549e33d471
e <l>
2019-05-20 00:04:15 +0800

    增加web包

--- src/main/java/com/easysui/starter/autoconfigure/EasySuiAutoConfigure.java
+++ src/main/java/com/easysui/starter/autoconfigure/EasySuiAutoConfigure.java
@@ -11,11 +11,17 @@ import com.easysui.starter.configuration.AspectConfiguration;
 import com.easysui.zookeeper.configuration.ZookeeperConfiguration;
 import com.easysui.zookeeper.configuration.ZookeeperProperties;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.curator.framework.CuratorFramework;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import redis.clients.jedis.JedisPool;
+
+import javax.sql.DataSource;
 
 /**
  * @author CHAO 2019/5/18 14:15
@@ -32,24 +38,29 @@ public class EasySuiAutoConfigure {
 
     @Bean
     @ConfigurationProperties("easysui.zookeeper")
+    @ConditionalOnClass(CuratorFramework.class)
     public ZookeeperProperties zookeeperProperties() {
         return new ZookeeperProperties();
     }
 
     @Bean
     @ConfigurationProperties("easysui.jedis")
+    @ConditionalOnClass(JedisPool.class)
     public JedisProperties jedisProperties() {
         return new JedisProperties();
     }
 
     @Bean
     @ConfigurationProperties("easysui.http")
+    //@ConditionalOnClass({HttpClient.class, RestTemplate.class})
     public RestTemplateProperties restTemplateProperties() {
         return new RestTemplateProperties();
     }
 
     @Bean
     @ConfigurationProperties("easysui.mybatis")
+    @ConditionalOnBean(DataSource.class)
+    //@ConditionalOnClass({SqlSessionFactory.class, SqlSessionFactoryBean.class})
     public MyBatisProperties myBatisProperties() {
         return new MyBatisProperties();
     }
